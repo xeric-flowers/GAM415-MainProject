1726c229c4049d2d99b74f4f7d856cbbc6fe1052
xeric-flowers <eric.floresx9@gmail.com>
2026-08-17 05:07:24 -0700

    Made Final Level incorporating all Stepping Stones

--- Source/GAM415_MainProject/PerlinProcTerrain.cpp
+++ Source/GAM415_MainProject/PerlinProcTerrain.cpp
@@ -60,7 +60,7 @@ void APerlinProcTerrain::CreateVertices()
 		for (int Y = 0; Y <= YSize; Y++)
 		{
 			float Z = FMath::PerlinNoise2D(FVector2D(X * NoiseScale + 0.1, Y * NoiseScale + 0.1)) * ZMultiplier;
-			GEngine->AddOnScreenDebugMessage(-1, 999.0f, FColor::Yellow, FString::Printf(TEXT("Z %f"), Z));
+			//GEngine->AddOnScreenDebugMessage(-1, 999.0f, FColor::Yellow, FString::Printf(TEXT("Z %f"), Z));
 			Vertices.Add(FVector(X * Scale, Y * Scale, Z));
 			UV0.Add(FVector2D(X * UVScale, Y * UVScale));
 		}
